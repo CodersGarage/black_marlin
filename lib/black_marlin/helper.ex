2e3520cb128a4980f3ac301ab9cf34e6e7d98d9f
Sakib Sami <s4kibs4mi@gmail.com>
2018-10-13 02:36:25 +0600

    - Added : Auto subscription

--- lib/black_marlin/helper.ex
+++ lib/black_marlin/helper.ex
@@ -1,7 +1,10 @@
 defmodule Helper do
 
   def send_subscribe_request(hash) do
-    :emqx_mgmt.subscribe(hash, make_topic_table(hash))
+    case :emqx_mgmt.subscribe(hash, make_topic_table(hash)) do
+      {error, reason} -> IO.inspect(["Subscription error = ", error, reason])
+      _ -> IO.inspect(["Subscription success"])
+    end
   end
 
   defp make_topic_table(hash) do
